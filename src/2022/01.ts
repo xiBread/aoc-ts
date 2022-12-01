1e22b86906c91a5d0465bfc5c709b5c40e30ded1
Oscar Lee-Vermeren <oscar.leevermeren@gmail.com>
2022-12-01 00:45:13 -0500

    asc sort + neg slice

--- src/2022/01.ts
+++ src/2022/01.ts
@@ -2,4 +2,4 @@ import aoc, { max, sum } from "../lib";
 
 const input = aoc.load((group) => sum(group.split("\n").map(Number)));
 
-export const day01 = () => [max(input), sum(input.sort((a, b) => b - a).slice(0, 3))];
+export const day01 = () => [max(input), sum(input.sort().slice(-3))];
